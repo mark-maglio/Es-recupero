28352a6bbc9051e694d1f149519925163c214e8b
mark maglio <markmaglio25@gmail.com>
2020-02-11 09:02:03 +0100

    primo commit dal mio pc

---
+++ Es recupero/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Es_recupero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
